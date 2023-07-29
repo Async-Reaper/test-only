acce0cb20728e33bc9bab0a43acd71a0b07fc55b
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2023-07-29 15:49:12 +0700

    Fix custom hook

--- src/shared/lib/hooks/useResize/useResize.ts
+++ src/shared/lib/hooks/useResize/useResize.ts
@@ -4,14 +4,15 @@ export const useResize = () => {
    const [width, setWidth] = React.useState(window.innerWidth);
 
    React.useEffect(() => {
-      const handleResize = (event: any) => {
-         setWidth(event.innerWidth);
+      const handleResize = (event: UIEvent) => {
+         const w = event.target as Window;
+         setWidth(w.innerWidth);
       };
       window.addEventListener('resize', handleResize);
       return () => {
          window.removeEventListener('resize', handleResize);
       };
-   }, []);
+   }, [setWidth]);
 
    return {
       width,
